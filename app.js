6f57b88776eaae8b5d171608267894712204e001
gtursi <gabrieltursi@gmail.com>
2014-11-27 16:51:09 -0300

    Initial version of CRUD services with REST

---
+++ app.js
@@ -0,0 +1,44 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require('mongoose');
+
+// Connection to DB
+mongoose.connect('mongodb://localhost:15247/tvshows', function(err, res) {
+  if(err) throw err;
+  console.log('Connected to Database');
+});
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// Import Models and controllers
+var models     = require('./models/tvshow')(app, mongoose);
+var TVShowCtrl = require('./controllers/tvshows');
+
+// Example Route
+var router = express.Router();
+router.get('/', function(req, res) {
+  res.send("Hello world!");
+});
+app.use(router);
+
+// API routes
+var tvshows = express.Router();
+
+router.get('/tvshows', TVShowCtrl.findAllTVShows);
+router.post('/tvshows', TVShowCtrl.addTVShow);
+
+router.get('/tvshows/:id', TVShowCtrl.findById);
+router.delete('/tvshows/:id', TVShowCtrl.deleteTVShow);
+router.put('/tvshows/:id', TVShowCtrl.updateTVShow);
+
+app.use('/api', tvshows);
+
+// Start server
+app.listen(15246, function() {
+  console.log("Node server running on http://localhost:15246");
+});
