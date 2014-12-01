14fa6211f770c86693603c431cac8a6c5a82ff85
gtursi <gabrieltursi@gmail.com>
2014-12-01 13:07:11 -0300

    Add Cross-Origin Resource Sharing

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(express.static(__dirname + '/public')); 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.header()
+	next();
+});
 
 // Import Models and controllers
 var models     = require('./models/tvshow')(app, mongoose);
--- controllers/tvshows.js
+++ controllers/tvshows.js
@@ -2,27 +2,27 @@ var mongoose = require('mongoose');
 var TVShow  = mongoose.model('TVShow');
 
 //GET - Return all tvshows in the DB
-exports.findAllTVShows = function(req, res) {
+module.exports.findAllTVShows = function(req, res) {
 	TVShow.find(function(err, tvshows) {
-    if(err) res.send(500, err.message);
-
-    console.log('GET /tvshows')
+    	if(err) res.send(500, err.message);
+	    console.log('GET /tvshows');
 		res.status(200).jsonp(tvshows);
 	});
 };
 
 //GET - Return a TVShow with specified ID
-exports.findById = function(req, res) {
+module.exports.findById = function(req, res) {
 	TVShow.findById(req.params.id, function(err, tvshow) {
     if(err) return res.send(500, err.message);
 
     console.log('GET /tvshow/' + req.params.id);
+		//res.setHeader('Access-Control-Allow-Origin', 'http://da03d:15246');
 		res.status(200).jsonp(tvshow);
 	});
 };
 
 //POST - Insert a new TVShow in the DB
-exports.addTVShow = function(req, res) {
+module.exports.addTVShow = function(req, res) {
 	console.log('POST');
 	console.log(req.body);
 
@@ -43,7 +43,7 @@ exports.addTVShow = function(req, res) {
 };
 
 //PUT - Update a register already exists
-exports.updateTVShow = function(req, res) {
+module.exports.updateTVShow = function(req, res) {
 	TVShow.findById(req.params.id, function(err, tvshow) {
 		tvshow.title   = req.body.title;
 		tvshow.year    = req.body.year;
@@ -61,7 +61,7 @@ exports.updateTVShow = function(req, res) {
 };
 
 //DELETE - Delete a TVShow with specified ID
-exports.deleteTVShow = function(req, res) {
+module.exports.deleteTVShow = function(req, res) {
 	TVShow.findById(req.params.id, function(err, tvshow) {
 		tvshow.remove(function(err) {
 			if(err) return res.send(500, err.message);
